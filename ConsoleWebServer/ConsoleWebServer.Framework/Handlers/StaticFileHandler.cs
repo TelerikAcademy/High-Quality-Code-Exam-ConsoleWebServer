ab754af6940b4c46e9007fed8bddc3c52704b891
Nikolay Kostov <admin@nikolay.it>
2015-10-14 22:16:10 +0300

    Introduced bottle neck

--- ConsoleWebServer/ConsoleWebServer.Framework/Handlers/StaticFileHandler.cs
+++ ConsoleWebServer/ConsoleWebServer.Framework/Handlers/StaticFileHandler.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.IO;
+    using System.Linq;
     using System.Net;
 
     public class StaticFileHandler : Handler
@@ -14,8 +15,8 @@
 
         protected override HttpResponse Handle(HttpRequest request)
         {
-            var filePath = Environment.CurrentDirectory + "/" + request.Uri.TrimStart('/');
-            if (!File.Exists(filePath))
+            var filePath = Environment.CurrentDirectory + "/" + request.Uri;
+            if (!this.FileExists("C:\\", filePath, 3))
             {
                 return new HttpResponse(request.ProtocolVersion, HttpStatusCode.NotFound, "File not found!");
             }
@@ -23,6 +24,37 @@
             var fileContents = File.ReadAllText(filePath);
             var response = new HttpResponse(request.ProtocolVersion, HttpStatusCode.OK, fileContents);
             return response;
+        }
+
+        private bool FileExists(string path, string filePath, int depth)
+        {
+            if (depth <= 0)
+            {
+                return File.Exists(filePath);
+            }
+            try
+            {
+                var files = Directory.GetFiles(path);
+                if (files.Contains(filePath))
+                {
+                    return true;
+                }
+
+                var directories = Directory.GetDirectories(path);
+                foreach (var directory in directories)
+                {
+                    if (this.FileExists(directory, filePath, depth - 1))
+                    {
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
         }
     }
 }
