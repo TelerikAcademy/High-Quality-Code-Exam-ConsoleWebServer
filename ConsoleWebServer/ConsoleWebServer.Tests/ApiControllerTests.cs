edec05a01e60be264f76ec50719922ca810c611f
Nikolay Kostov <admin@nikolay.it>
2015-10-15 21:58:49 +0300

    Tests for ApiController.ReturnMe added with mocking

--- ConsoleWebServer/ConsoleWebServer.Tests/ApiControllerTests.cs
+++ ConsoleWebServer/ConsoleWebServer.Tests/ApiControllerTests.cs
@@ -1,5 +1,8 @@
 ﻿namespace ConsoleWebServer.Tests
 {
+    using System;
+    using System.Linq;
+
     using ConsoleWebServer.Application.Controllers;
     using ConsoleWebServer.Framework;
 
@@ -14,10 +17,22 @@
         public void ReturnMeActionReturnsParameterInTheJsonObject()
         {
             const string Parameter = "someParam123";
-            var request = new Mock<IHttpRequest>("GET", "/", "1.1");
+            var request = new Mock<IHttpRequest>();
+            request.Setup(x => x.ProtocolVersion).Returns(new Version(1, 1));
             var actionResult = new ApiController(request.Object);
             var resultBody = actionResult.ReturnMe(Parameter).GetResponse().Body;
             Assert.AreEqual(string.Format("{{\"param\":\"{0}\"}}", Parameter), resultBody);
+        }
+
+        [TestMethod]
+        public void ReturnMeActionReturnsJsonContentType()
+        {
+            const string Parameter = "someParam123";
+            var request = new Mock<IHttpRequest>();
+            request.Setup(x => x.ProtocolVersion).Returns(new Version(1, 1));
+            var actionResult = new ApiController(request.Object);
+            var contentType = actionResult.ReturnMe(Parameter).GetResponse().Headers["Content-Type"].First();
+            Assert.AreEqual("application/json", contentType);
         }
 
         // TODO: Add unit tests for ReturnMe and GetDateWithCors
